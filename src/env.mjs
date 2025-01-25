4b26fdf9db3af5a8d9846f5ac69d64bd6fe99724
PhilO1 <9023199-PhilO1@users.noreply.replit.com>
2025-01-25 23:17:47 +0000

    User checkpoint: Refactor environment variable schema and runtimeEnv type.

--- src/env.mjs
+++ src/env.mjs
@@ -1,34 +1,33 @@
-
 import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
-const portSchema = z.preprocess(
-  (val) => {
-    const processed = z.string().safeParse(val);
-    return processed.success ? parseInt(processed.data, 10) : 3001;
-  },
-  z.number().min(1).max(65535)
-);
+const portSchema = z.preprocess((val): number => {
+ const processed = z.string().safeParse(val);
+ return processed.success ? parseInt(processed.data, 10) : 3001;
+}, z.number().min(1).max(65535));
 
 export const env = createEnv({
-  server: {
-    NODE_ENV: z.enum(['development', 'test', 'production']),
-    PORT: portSchema,
-    REDIS_HOST: z.string().default('0.0.0.0'),
-    REDIS_PORT: portSchema.default(6379),
-    REDIS_PASSWORD: z.string().optional(),
-  },
-  client: {
-    NEXT_PUBLIC_APP_URL: z.string().url(),
-    NEXT_PUBLIC_WEBSOCKET_URL: z.string().url().default(`ws://0.0.0.0:${process.env.PORT || '3001'}/ws`),
-  },
-  runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    PORT: process.env.PORT,
-    REDIS_HOST: process.env.REDIS_HOST,
-    REDIS_PORT: process.env.REDIS_PORT,
-    REDIS_PASSWORD: process.env.REDIS_PASSWORD,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-    NEXT_PUBLIC_WEBSOCKET_URL: process.env.NEXT_PUBLIC_WEBSOCKET_URL,
-  }
+ server: {
+   NODE_ENV: z.enum(['development', 'test', 'production']),
+   PORT: portSchema,
+   REDIS_HOST: z.string().default('0.0.0.0'),
+   REDIS_PORT: portSchema.default(6379),
+   REDIS_PASSWORD: z.string().optional(),
+ },
+ client: {
+   NEXT_PUBLIC_APP_URL: z.string().url(),
+   NEXT_PUBLIC_WEBSOCKET_URL: z
+     .string()
+     .url()
+     .default(`ws://0.0.0.0:${process.env.PORT ?? '3001'}/ws`),
+ },
+ runtimeEnv: process.env as {
+   NODE_ENV: string;
+   PORT: string;
+   REDIS_HOST: string;
+   REDIS_PORT: string;
+   REDIS_PASSWORD?: string;
+   NEXT_PUBLIC_APP_URL: string;
+   NEXT_PUBLIC_WEBSOCKET_URL: string;
+ },
 });
