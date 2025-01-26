9877c05fea1ff0e8661fa0e764245b490f3565cb
Phil <philip.a.oshea@gmail.com>
2025-01-26 22:57:21 +0000

    route 4

--- app/api/auth/upgrade-eligibility/[userId]/route.ts
+++ app/api/auth/upgrade-eligibility/[userId]/route.ts
@@ -8,7 +8,7 @@ export async function GET(
 ) {
   try {
     const user = await prisma.user.findUnique({
-      where: { id: context.params.userId },
+      where: { id: request.params.userId },
       include: {
         safetyChecks: true,
         matches: true,
